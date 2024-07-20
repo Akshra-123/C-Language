170fb0a5200560156592dc4a11c34f03ed2d6486
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-07-20 23:11:24 +0530

    Create HelloWorld.c

---
+++ Basics/HelloWorld.c
@@ -0,0 +1,8 @@
+# include<stdio.h> // stdio.h is a preprocessor directive 
+int main() {
+    printf("Hello World!");
+    return 0;
+    /* 
+    we can write multi-line comments this way
+    */
+}
