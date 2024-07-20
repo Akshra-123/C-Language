e337a4e7f90265ea0e4e53129d1ad2eda3c0c571
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-07-20 23:12:19 +0530

    Add files via upload

---
+++ Basics/variables.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main() {
+    int a = 25;
+    int A = 90; // shows the case-sensitivity of variables
+    int _age = 34; // can start a variable name either with an alphabet or underscore and nothing else
+    int student_marks=90; // the only special character used in a variable name is underscore
+    char name = 't';
+    float pie = 3.14;
+    printf("%d\n",a);
+    printf("%d\n",A);
+    printf("Age is: %d\n",_age);
+    printf("%c\n",name);
+    printf("%f\n",pie);
+
+
+}
