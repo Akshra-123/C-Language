a5e5323550ea1c3ca5e89f760146ae4808b914f8
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-07-20 23:14:44 +0530

    Add files via upload

---
+++ Basics/SumTwo.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main() {
+    int a; 
+    int b;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter Second number:");
+    scanf("%d",&b);
+    printf("Sum is: %d",a+b);
+}
